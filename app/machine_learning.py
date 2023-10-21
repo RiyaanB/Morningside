ca5f396275eb5647be8f4ea4f5075c1dfac24966
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2023-10-21 16:21:56 -0400

    Custom ML model option in Machine Learning

--- app/machine_learning.py
+++ app/machine_learning.py
@@ -12,6 +12,19 @@ def show_machine_learning():
     model_options = ["Model A", "Model B", "Model C", "Model D"]
     selected_model = st.selectbox("Available models", model_options)
 
+    # Option to upload or write your own model
+    st.write("Or...")
+    upload_write_model_option = st.radio("Upload or write your own model", ["Upload My Model", "Write My Model"])
+
+    if upload_write_model_option == "Upload My Model":
+        uploaded_model = st.file_uploader("Upload your ML model file", type=["py", "ipynb"])
+        if uploaded_model is not None:
+            st.success("Model uploaded successfully!")
+    else:
+        model_code = st.text_area("Write your ML model code here")
+        if model_code:
+            st.success("Model code received!")
+
     # Data loader fine-tuning
     st.write("Fine-tune your data loader:")
     batch_size = st.slider("Batch size", 1, 100, 32)
@@ -77,47 +90,3 @@ def show_machine_learning():
                     f.write("Fake model weights")
                 with open("model_weights.txt", "rb") as f:
                     st.download_button("Download", f, file_name="model_weights.txt")
-# Import Streamlit
-import streamlit as st
-import numpy as np
-
-def show_results():
-    # Page title
-    st.title("Results")
-
-    # Option to upload test data or use already uploaded data
-    st.write("Upload test data or use already uploaded data:")
-    test_data_option = st.radio("Select test data option", ["Upload new test data", "Use already uploaded data"])
-
-    if test_data_option == "Upload new test data":
-        test_data = st.file_uploader("Upload your test data", accept_multiple_files=True)
-    else:
-        st.write("Using already uploaded data")
-
-    # Button to make predictions
-    make_predictions_button = st.button("Make Predictions")
-
-    if make_predictions_button:
-        # Estimated time and price for predictions
-        time = np.random.uniform(0.5, 2.0)
-        price = np.random.uniform(1, 5)
-        st.write(f"Estimated time for predictions: {time:.2f} hours")
-        st.write(f"Estimated price for predictions: ${price:.2f}")
-
-        # Confirmation for predictions
-        confirm_predictions = st.button("Confirm and Start Predictions")
-
-        if confirm_predictions:
-            st.write("Computing predictions...")
-            st.progress(100)
-            st.success("Predictions completed!")
-
-            # Option to download predictions
-            st.write("Download Predictions:")
-            download_button = st.button("Download")
-            if download_button:
-                # Generate a file with fake predictions and provide a link for download
-                with open("predictions.txt", "w") as f:
-                    f.write("Fake predictions")
-                with open("predictions.txt", "rb") as f:
-                    st.download_button("Download", f, file_name="predictions.txt")
